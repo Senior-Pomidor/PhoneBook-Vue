cdf179330319fa4326bd636f73176cbb287393ff
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-11-14 15:30:11 +0300

    added contacts module to store

---
+++ src/store/contacts.js
@@ -0,0 +1,25 @@
+export default {
+	state: {
+		contacts: [
+			{
+				id: '1',
+				name: 'Dow John',
+				phone: '+7 999 228 14 88',
+				email: 'dowjohn@gmail.com'
+			}, {
+				id: '2',
+				name: 'Dow John 2',
+				phone: '+7 999 228 14 88',
+				email: 'dowjohn@gmail.com'
+			}, {
+				id: '3',
+				name: 'Dow John 3',
+				phone: '+7 999 228 14 88',
+				email: 'dowjohn@gmail.com'
+			}
+		]
+	},
+	getters: {
+		contacts: state => state.contacts
+	}
+}
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -24,24 +24,7 @@ export default {
   data() {
     return {
       isOpenForm: false,
-      contacts: [
-        {
-          id: '1',
-          name: 'Dow John',
-          phone: '+7 999 228 14 88',
-          email: 'dowjohn@gmail.com'
-        }, {
-          id: '2',
-          name: 'Dow John 2',
-          phone: '+7 999 228 14 88',
-          email: 'dowjohn@gmail.com'
-        }, {
-          id: '3',
-          name: 'Dow John 3',
-          phone: '+7 999 228 14 88',
-          email: 'dowjohn@gmail.com'
-        }
-      ]
+      contacts: this.$store.getters.contacts
     }
   },
   methods: {
