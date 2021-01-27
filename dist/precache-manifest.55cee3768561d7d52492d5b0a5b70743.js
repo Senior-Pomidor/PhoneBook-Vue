d887d041fb5298c629568ad344b5a673219e64ae
Senior-Pomidor <bobylev.vladi@gmail.com>
2021-01-28 00:39:41 +0300

    build

---
+++ dist/precache-manifest.55cee3768561d7d52492d5b0a5b70743.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7040f7a9af38a59e6875",
+    "url": "/css/app.4b9e52e1.css"
+  },
+  {
+    "revision": "23bf599db97c9c3a1708",
+    "url": "/css/chunk-1be0369e.7ee0c580.css"
+  },
+  {
+    "revision": "8c781d35845d745b67d7",
+    "url": "/css/chunk-6f5f39b7.d9a9ff17.css"
+  },
+  {
+    "revision": "ddc27ae942f03d8fd5f18921ee078f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7040f7a9af38a59e6875",
+    "url": "/js/app.8f76b5c5.js"
+  },
+  {
+    "revision": "23bf599db97c9c3a1708",
+    "url": "/js/chunk-1be0369e.138d083d.js"
+  },
+  {
+    "revision": "8c781d35845d745b67d7",
+    "url": "/js/chunk-6f5f39b7.c6bb1071.js"
+  },
+  {
+    "revision": "0aa2de67b5f7a65aba82",
+    "url": "/js/chunk-vendors.a2ac9e55.js"
+  },
+  {
+    "revision": "9e2017819352feeae0c0df1166bda53c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
