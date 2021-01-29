37f3b2698f1714545d8c1df57dae98aeac2b2726
Senior-Pomidor <bobylev.vladi@gmail.com>
2021-01-29 14:47:41 +0300

    deploy

---
+++ precache-manifest.19310b1a6ebed82984ea95cdd8a162aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd40519bf6aee7cec20",
+    "url": "/PhoneBook-Vue/css/app.adb6e1d9.css"
+  },
+  {
+    "revision": "dcaf22ba0820b1bc886d",
+    "url": "/PhoneBook-Vue/css/chunk-19784763.a9f315ff.css"
+  },
+  {
+    "revision": "bf57889241a2b0c79919",
+    "url": "/PhoneBook-Vue/css/chunk-bec4bed6.c0a04900.css"
+  },
+  {
+    "revision": "a82bbed9d2f43b3c6b8737c005d7ed62",
+    "url": "/PhoneBook-Vue/index.html"
+  },
+  {
+    "revision": "3bd40519bf6aee7cec20",
+    "url": "/PhoneBook-Vue/js/app.dc1a384d.js"
+  },
+  {
+    "revision": "dcaf22ba0820b1bc886d",
+    "url": "/PhoneBook-Vue/js/chunk-19784763.affbb603.js"
+  },
+  {
+    "revision": "bf57889241a2b0c79919",
+    "url": "/PhoneBook-Vue/js/chunk-bec4bed6.27af31ea.js"
+  },
+  {
+    "revision": "0aa2de67b5f7a65aba82",
+    "url": "/PhoneBook-Vue/js/chunk-vendors.a2ac9e55.js"
+  },
+  {
+    "revision": "606bb4092981503f893edf681237f3ac",
+    "url": "/PhoneBook-Vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PhoneBook-Vue/robots.txt"
+  }
+]);
